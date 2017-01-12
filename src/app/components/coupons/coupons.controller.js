6840143b65ac640142d4a2359341f6789c28bb42
Leonardo Moreira <eu@leonardomoreira.com.br>
2017-01-12 19:27:45 +0000

    coupons

---
+++ src/app/components/coupons/coupons.controller.js
@@ -0,0 +1,16 @@
+app.controller('coupons.controller', [
+    '$scope',
+    '$location',
+    '$routeParams',
+    '$route',
+    '$timeout',
+    '$filter',
+    '$window',
+    '$http',
+    '$httpParamSerializer', function($scope, $location, $routeParams, $route, $timeout, $filter, $window, $http, $httpParamSerializer) {
+
+    console.log("Coupons");
+
+    var base = angular.element("#coupons");
+
+}]);
--- src/app/core/routes.js
+++ src/app/core/routes.js
@@ -12,6 +12,11 @@ angular.module('core.routes', ['ngRoute']).config([
             controller  : 'home.controller'
         })
 
+        .when('/coupons', {
+            templateUrl : 'views/coupons/coupons.html',
+            controller  : 'coupons.controller'
+        })
+
         .when('/offer/:slug', {
             templateUrl : 'views/offer/offer.html',
             controller  : 'offer.controller'
